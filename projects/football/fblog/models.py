f3dbf015bee2bbccfdf7993fffa425878992cf57
danielsind <danielsindet@gmail.com>
2023-05-29 10:09:02 +0300

    Commented out the Author field it is clashing with the user

--- projects/football/fblog/models.py
+++ projects/football/fblog/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
 
-class Author(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    bio = models.TextField()
+# class Author(models.Model):
+   # user = models.OneToOneField(User, on_delete=models.CASCADE)
+   ## profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     post_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
