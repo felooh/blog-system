60e9e06a45b9367a0677368366518e2dcf081a8b
danielsind <danielsindet@gmail.com>
2023-05-30 15:46:56 +0300

    Added image field in Post model

--- projects/football/fblog/models.py
+++ projects/football/fblog/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(upload_to='static/fblog/images')
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
