b1a636631dc5e69ee8e3088797b27d15916394a8
danielsind <danielsindet@gmail.com>
2023-05-30 12:08:13 +0300

    Edited the Models recreated the model for Post

--- projects/football/fblog/models.py
+++ projects/football/fblog/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse 
 from django.contrib.auth.models import User
-from django.db import models
-
-# class Author(models.Model):
-   # user = models.OneToOneField(User, on_delete=models.CASCADE)
-   ## profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    post_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+            return self.title
+
+    def get_absolute_url(self):
+            return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+
 
