64f90fc3b2ee43ea91dc96f2102ae7268afb71bc
felix <gathagefelix@gmail.com>
2023-05-30 16:42:41 +0300

    updated the league images. player images to be updated by Daniel

--- projects/football/fblog/urls.py
+++ projects/football/fblog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-
 from . import views
+from .views import PostListView, PostDetailView, PostCreate, PostUpdate, PostDeleteView
+
 
 urlpatterns = [
-   
+    path('', views.index, name= "index"),
+    path('posts/', PostListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>', PostDetailView.as_view(), name = 'post-detail'),
+    path('post/<int:pk>/update', PostUpdate.as_view(), name = 'post-update'),
+    path('post/new', PostCreate.as_view(), name = 'post-create'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name = 'post-delete'),
 ]
