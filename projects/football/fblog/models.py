b69207f1b78ffd90585cfbe0de4f3efec1de641d
danielsind <danielsindet@gmail.com>
2023-05-31 15:28:17 +0300

    Good good

--- projects/football/fblog/models.py
+++ projects/football/fblog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to='images/', null=True, blank=True)
-    date_posted = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
